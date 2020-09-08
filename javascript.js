b44c32b79da3f14b5eb7f30b9bb5217d6f9391be
Bernatto <braianbernatto@hotmail.com>
2020-09-07 22:57:36 -0400

    javascript thing

---
+++ javascript.js
@@ -0,0 +1,18 @@
+function especialidades() {
+    console.log('se presiono especialidades');
+    console.log(document.getElementById("especialidades_list").style.display);
+    var estado =  document.getElementById("especialidades_list");
+
+        estado.style.display = "grid";
+        document.getElementById("profesionales_list").style.display = "none";
+        console.log(document.getElementById("especialidades_list").style.display);
+} 
+function profesionales() {
+    console.log('se presiono profesionales');
+    console.log(document.getElementById("profesionales_list").style.display);
+    var estado =  document.getElementById("profesionales_list");
+
+        estado.style.display = "grid";
+        document.getElementById("especialidades_list").style.display = "none";
+    console.log(document.getElementById("profesionales_list").style.display);
+   } 
