395f95821221c9dda192845be80845de0ac58ac4
Bernatto <braianbernatto@hotmail.com>
2020-09-15 17:20:33 -0400

    turnos add

--- javascript.js
+++ javascript.js
@@ -1,9 +1,24 @@
+turnos()
+
+function inicio() {
+    console.log('se presiono inicio');
+    var estado = document.getElementById("inicio");
+    estado.style.display = "grid";
+    document.getElementById("profesionales_list").style.display = "none";
+    document.getElementById("estudios_list").style.display = "none";
+    document.getElementById("turnos").style.display = "none";
+    document.getElementById("especialidades_list").style.display = "none";
+    
+}
+
 function especialidades() {
     console.log('se presiono especialidades');
     var estado = document.getElementById("especialidades_list");
     estado.style.display = "grid";
     document.getElementById("profesionales_list").style.display = "none";
     document.getElementById("estudios_list").style.display = "none";
+    document.getElementById("turnos").style.display = "none";
+    document.getElementById("inicio").style.display = "none";
 }
 function estudios() {
     console.log('se presiono estudios');
@@ -11,6 +26,8 @@ function estudios() {
     estado.style.display = "grid";
     document.getElementById("especialidades_list").style.display = "none";
     document.getElementById("profesionales_list").style.display = "none";
+    document.getElementById("turnos").style.display = "none";
+    document.getElementById("inicio").style.display = "none";
 }
 function profesionales() {
     console.log('se presiono profesionales');
@@ -18,14 +35,15 @@ function profesionales() {
     estado.style.display = "grid";
     document.getElementById("especialidades_list").style.display = "none";
     document.getElementById("estudios_list").style.display = "none";
+    document.getElementById("turnos").style.display = "none";
+    document.getElementById("inicio").style.display = "none";
 }
 function turnos() {
     console.log('se presiono turnos');
-    var estado = document.getElementById("turnos_list");
+    var estado = document.getElementById("turnos");
     estado.style.display = "grid";
     document.getElementById("especialidades_list").style.display = "none";
     document.getElementById("estudios_list").style.display = "none";
     document.getElementById("profesionales_list").style.display = "none";
+    document.getElementById("inicio").style.display = "none";
 }
-
-
