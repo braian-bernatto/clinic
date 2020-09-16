e8d39eef362c14e619695483fc2683b8b635f0d6
Bernatto <braianbernatto@hotmail.com>
2020-09-16 14:08:57 -0400

    progress bar

--- javascript.js
+++ javascript.js
@@ -47,3 +47,68 @@ function turnos() {
     document.getElementById("profesionales_list").style.display = "none";
     document.getElementById("inicio").style.display = "none";
 }
+
+function t_formulario1() {
+    var estado = document.getElementById("turnos_formulario");
+    estado.style.display = "flex";
+    document.getElementById("turnos_formulario2").style.display = "none";
+    document.getElementById("turnos_formulario3").style.display = "none";
+    document.getElementById("turnos_formulario4").style.display = "none";
+}
+
+function t_formulario2() {
+    var estado = document.getElementById("turnos_formulario2");
+    estado.style.display = "flex";
+    document.getElementById("turnos_formulario").style.display = "none";
+    document.getElementById("turnos_formulario3").style.display = "none";
+    document.getElementById("turnos_formulario4").style.display = "none";
+
+
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+}
+
+function t_formulario3() {
+    var estado = document.getElementById("turnos_formulario3");
+    estado.style.display = "flex";
+    document.getElementById("turnos_formulario").style.display = "none";
+    document.getElementById("turnos_formulario2").style.display = "none";
+    document.getElementById("turnos_formulario4").style.display = "none";
+
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+}
+
+function t_formulario4() {
+    var estado = document.getElementById("turnos_formulario4");
+    estado.style.display = "flex";
+    document.getElementById("turnos_formulario").style.display = "none";
+    document.getElementById("turnos_formulario2").style.display = "none";
+    document.getElementById("turnos_formulario3").style.display = "none";
+
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+}
+
+const progressText = document.querySelectorAll(".step p");
+const bullet = document.querySelectorAll(".step .bullet");
+const progressCheck = document.querySelectorAll(".step i");
+let max = 4;
+let current = 1;
+
+
+
+
+// const next_btn_form1 = document.querySelector(".form1_next");
+
+// next_btn_form1.addEventListener("click", function(){
+//     var estado = document.getElementById("turnos_formulario2");
+//     estado.style.display = "flex";
+//     document.getElementById("turnos_formulario").style.display = "none";
+// });
